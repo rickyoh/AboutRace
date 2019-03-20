e7849e5af0b30086d6cf68f918e4e4ade31d135f
Eric Hildebrand <eric@blackantelope.com>
2019-03-20 10:40:46 -0700

    add Google analytics gattby module and configure

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,5 +20,12 @@ module.exports = {
         path: `${__dirname}/gatsby-config.js`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-136620314-1",
+      },
+    },
   ],
 }
